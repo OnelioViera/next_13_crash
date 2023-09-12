95b04bd7ca373691c2f32a482dfe225b5cd76cd5
Onelio Viera <ojvwebdesign@gmail.com>
2023-09-12 14:01:44 -0400

    update files

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,7 @@ const HomePage = () => {
     <div>
       <h1>Welcome</h1>
       <h2>Onelio Viera</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit asperiores consequuntur quam! Rem recusandae commodi culpa consectetur necessitatibus perspiciatis possimus facere. Voluptate praesentium inventore laudantium repellendus eum similique eos corporis.</p>
     </div>
     )
 }
