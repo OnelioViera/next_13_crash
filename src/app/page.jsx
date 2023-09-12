8d1759799b0446088c8bb47aceb197c7cb89b7b4
Onelio Viera <ojvwebdesign@gmail.com>
2023-09-12 11:51:58 -0400

    update files

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,15 +2,10 @@ import Link from 'next/link'
 
 const HomePage = () => {
   return (
+    
     <div>
       <h1>Welcome</h1>
       <h2>Onelio Viera</h2>
-      {/* <ul>
-        <li><Link href='/'>Home</Link></li>
-        <li><Link href='/about'>About</Link></li>
-        <li><Link href='/about/team'>Team</Link></li>
-
-      </ul> */}
     </div>
     )
 }
