1429d95f0c0ce7c289dace1adc1ccc789ca078e0
Onelio Viera <ojvwebdesign@gmail.com>
2023-09-11 20:43:35 -0400

    update files

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto doloremque soluta magni placeat similique. Sint, totam id nisi reprehenderit beatae error quasi ullam repellat itaque blanditiis repellendus distinctio! Ipsam numquam, neque aut fugiat placeat id corporis obcaecati laboriosam beatae qui. Exercitationem voluptatum, ea dolores quaerat veniam maiores reiciendis incidunt eaque?</p>
+    </div>
+  )
+}
+
+export default AboutPage;
+
---
+++ src/app/about/team/page.jsx
@@ -0,0 +1,11 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum incidunt quos, ipsa repellat ut omnis nobis dolore error sunt perferendis harum deleniti velit inventore provident, veniam nihil eos maxime molestiae!
+      </p>
+    </div>
+  )
+}
+
+export default TeamPage
